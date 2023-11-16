afb6ae7430f2862808e188c643325200c7a7e5b8
Preston-Wong <you@example.com>
2023-11-16 11:03:49 -0500

    linked parabola and dropping animation

--- Sketch.java
+++ Sketch.java
@@ -13,7 +13,7 @@ public class Sketch extends PApplet {
   PImage ImgClouds;
 
   // location and speed variables for the circle
-	float floatX = 465; // 200 and 800 start
+	float floatX = 320; // 320, 800
   float floatY = 800; // 730 and 475 end
   float floatXSpeed = 1;
   float floatYSpeed = 1;
@@ -46,10 +46,10 @@ public class Sketch extends PApplet {
    */
   public void draw() {
     // prints background right before printout the the new circle location 
-   // image(ImgBackground,0,0);
+    image(ImgBackground,0,0);
 
     // prints out the clousd 
-   // image(ImgClouds, floatXPic, floatYPic);
+    image(ImgClouds, floatXPic, floatYPic);
 
     // prints out the cloud and moves them from left to right
     floatXPic = floatXPic + floatXPicSpeed;
@@ -66,34 +66,23 @@ public class Sketch extends PApplet {
     noStroke();
     ellipse(floatX, floatY,50, 50);
 
-    // adjust the circle location 
-    if (floatX <= 465) {
-      //floatX = floatX + floatXSpeed;
-      //floatY = floatY - (float) (0.0001 * Math.pow((-floatX + 550),2)); ;
-    } else if (floatX >= 465) {
-      floatX = floatX + floatXSpeed;
-      floatY = floatY + (float) (0.0001 * Math.pow(floatX - 550,2));
-    }
+  floatX = floatX + floatXSpeed;    
+  floatY = floatY + floatYSpeed;
 
-      /*
-    // detects if the ball has hit the hoop, and it it has, it cycles through some movements to get back to the starting position
-    if (floatX >= 730 && (floatY >= 475 && floatY < 900)) {
+    // adjust the circle location 
+    if ( floatX >= 320 && floatX <= 570 && floatY <= 900) {
+      floatYSpeed = (float) -(0.0001 * Math.pow((-floatX + 570),2));
+    } else if (floatX >= 570 && floatX <= 730) {
+      floatYSpeed = (float) (0.0001 * Math.pow((-floatX + 570),2));
+    } else if (floatX >= 730 && floatY >= 475 && floatY < 900) {
       floatYSpeed = 5;
       floatXSpeed = 0;
-    } else if (floatX >= 730 && floatY <= 900) {
+    } else if (floatX >= 730 && floatY >= 900) {
       floatYSpeed = 0;
       floatXSpeed = -5;
-    } else if (floatX <= 100 && (floatY <= 900 && floatY >= 800)) {
+    } else if (floatX <= 320 && floatY <= 800 ) {
       floatXSpeed = 0;
-      floatYSpeed = -1;
+     floatYSpeed = -1;
     }
-    
-
-    // moves the ball in the parabolic shape for the shot
-    if (floatX <= 100 && floatY <= 800) {
-      floatYSpeed = 0;
-      floatXSpeed = 0;
-    }
-    */
   }
-}
+  }
